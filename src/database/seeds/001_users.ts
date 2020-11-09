cfbdb69cc9326b2da899bd81820dfc5ef151f01e
FelipeColona <72169708+FelipeColona@users.noreply.github.com>
2020-11-08 22:35:18 -0300

    revert: revert and fix the generation of hashes in the users seed
    The genHash function was not working, it was returning an empty string, so i simplified and just
    awaited for the own bcrypt function

--- src/database/seeds/001_users.ts
+++ src/database/seeds/001_users.ts
@@ -1,18 +1,6 @@
 import * as Knex from "knex";
 import * as bcrypt from 'bcrypt' 
 
-function genHash(){
-    let hashedPassword = ''
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash('senha', salt, (err, hash) => {
-            hashedPassword = hash
-        })
-    })
-    return hashedPassword
-}
-
-
-
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex('users').del();
@@ -22,52 +10,52 @@ export async function seed(knex: Knex): Promise<void> {
         { 
             name: 'Felipe Ferreira Colona',
             email: 'felipe@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Daniel Shiffman',
             email: 'shiffman@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Bill Cipher',
             email: 'bill.cipher@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Hollow Knight',
             email: 'hollow.knight@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Cuphead Mugman',
             email: 'cuphead.mugman@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Pale King',
             email: 'pale.king@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Dipper Pines',
             email: 'pipper.pines@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Mabel Pines',
             email: 'mabel.pines@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Stanford Pines',
             email: 'stanford.pines@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         },
         { 
             name: 'Stanley Pines',
             email: 'stanley.pines@gmail.com',
-            password: genHash(),
+            password: await bcrypt.hash('password', 10),
         }
     ]);
 };
